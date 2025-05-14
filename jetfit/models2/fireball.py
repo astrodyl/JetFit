Fix MAC Flux Normalization

jetfit/models2/fireball.py
@@ -167,6 +167,12 @@ class StratifiedFireballModel(BaseFireballModel):
         if not self.is_valid:
             return np.array([np.nan])
 
+        if self.sn < 0 and self.k1 < self.k2:
+            return np.array([np.nan])
+
+        if self.sn > 0 and self.k1 > self.k2:
+            return np.array([np.nan])
+
         # For speed, get observation as arrays
         arrays = obs.as_arrays
 
@@ -174,14 +180,18 @@ class StratifiedFireballModel(BaseFireballModel):
         n, k = self.smooth(arrays.times)
 
         # Modeled flux smoothed across breaks/regimes
-        modeled = ObservedSpectrumModel(
+        obs_spectrum = ObservedSpectrumModel(
             **self.spectrum(arrays.times, n, k), arrays=arrays
-        ).model()
+        )
+        modeled = obs_spectrum.model()
+
+        if np.isnan(modeled).any():
+            return modeled
 
         if self.tj:
             # Jet break spectrum
             jet_flux = ObservedSpectrumModel(
-                **self.spectrum(self.tj, n, k), arrays=arrays
+                **self.spectrum(self.tj), arrays=arrays, fts=obs_spectrum.has_fts
             ).model()
 
             modeled[obs.flux_loc] = self.smooth_jet_break(
@@ -579,6 +589,9 @@ class FireballModel(BaseFireballModel):
             **self.spectrum(arrays.times), arrays=arrays  # type: ignore
         )
         modeled = obs_spectrum.model(subset)
+
+        if np.isnan(modeled).any():
+            return modeled
 
         if self.tj:
             # Jet break spectrum

test/models/test_spectral_flux.py
@@ -9,14 +9,53 @@ from jetfit.models2.basemodels import SpectralFluxModel, fast_to_slow_time
 
 
 class TestSpectralFlux(unittest.TestCase):
-    def test_ftz_time(self):
+    def test_MAC(self):
         """"""
-        with open(r"C:\Users\Dylan\Downloads\best_fit.json", "r") as jf:
-            data = json.load(jf)
+        # Define the frequencies
+        nu_m, nu_a, nu_c = 1e9, 1e11, 1e13
+        nu = np.geomspace(1e8, 1e18, 500)
 
+        # Define other
+        f_peak, p, k = 1, 2.5, 0.0
 
+        # Get smoothed flux
+        model = SpectralFluxModel(nu_m, nu_c, nu_a, f_peak, p, k)
 
-        print()
+        # Get SPN98 flux
+        b1, b2, b3 = model.spectral_indices()
+
+        smoothed_flux = model(nu) #* (nu_a / nu_m) ** b2
+
+        # Determine the segments
+        seg0 = nu <= nu_m
+        seg1 = np.logical_and(nu > nu_m, nu <= nu_c)
+        seg2 = np.logical_and(nu_a < nu, nu_c >= nu_m)
+        seg3 = nu > nu_c
+
+        # Calculate the sharply-broken flux
+        flux = np.empty(nu.size)
+        flux[seg0] = f_peak * (nu_m / nu_a) ** ((p + 4) / 2) * (nu[seg0] / nu_m) ** 2
+        flux[seg1] = f_peak * (nu_a / nu_m) ** ((1 - p) / 2) * (nu[seg1] / nu_a) ** 2.5
+        flux[seg2] = f_peak * (nu[seg2] / nu_m) ** ((1 - p) / 2)
+        flux[seg3] = f_peak * (nu_c / nu_m) ** ((1 - p) / 2) * (nu[seg3] / nu_c) ** (-p / 2)
+
+        # Annotation for each break
+        plt.annotate(r'$\nu_m$', xy=(1.2 * nu_m, 1e-9), xytext=(1.2 * nu_m, 1e-9), fontsize=12)
+        plt.annotate(r'$\nu_a$', xy=(1.2 * nu_a, 1e-9), xytext=(1.2 * nu_a, 1e-9), fontsize=12)
+        plt.annotate(r'$\nu_c$', xy=(1.2 * nu_c, 1e-9), xytext=(1.2 * nu_c, 1e-9), fontsize=12)
+
+        # Plot the two
+        plt.vlines(nu_m, ymin=0.0, ymax=f_peak * (nu_m / nu_a) ** ((p + 4) / 2), color='black', linestyle='--', alpha=0.6)
+        plt.vlines(nu_a, ymin=0.0, ymax=f_peak * (nu_a / nu_m) ** b2, color='black', linestyle='--', alpha=0.6)
+        plt.vlines(nu_c, ymin=0.0, ymax=f_peak * (nu_c / nu_m) ** b2, color='black', linestyle='--', alpha=0.6)
+        plt.title(r'$\nu_m < \nu_a < \nu_c$', fontsize=18)
+        plt.xlabel('Frequency [Hz]')
+        plt.ylabel('Flux [mJy]')
+        plt.grid(alpha=0.5)
+        plt.loglog(nu, smoothed_flux, label='Smoothly Broken')
+        plt.loglog(nu, flux, label='Sharply Broken')
+        plt.legend()
+        plt.show()
 
     def test_slow_cooling_spectrum(self):
         """
@@ -51,6 +90,48 @@ class TestSpectralFlux(unittest.TestCase):
         plt.vlines(nu_m, ymin=0.0, ymax=f_peak, color='black', linestyle='--', alpha=0.6)
         plt.vlines(nu_c, ymin=0.0, ymax=f_peak * (nu_c / nu_m) ** b2, color='black', linestyle='--', alpha=0.6)
         plt.title('Slow Cooling Spectral Flux')
+        plt.loglog(nu, smoothed_flux, label='Smoothly Broken')
+        plt.loglog(nu, flux, label='Sharply Broken')
+        plt.legend()
+        plt.show()
+
+    def test_slow_cooling_spectrum(self):
+        """
+        Visual inspection of slow-cooling smoothing approximation.
+        """
+        # Define the frequencies
+        nu_a, nu_m, nu_c = 1e12, 1e10, 1e14
+        nu = np.geomspace(1e8, 1e18, 500)
+
+        # Define other
+        f_peak, p, k = 2e4, 2.5, 0.0
+
+        # Get smoothed flux
+        model = SpectralFluxModel(nu_m, nu_c, nu_a, f_peak, p, k)
+        smoothed_flux = model(nu)
+
+        # Get SPN98 flux
+        b1, b2, b3 = model.spectral_indices()
+
+        # Determine the segments
+        seg_a = nu < nu_m
+        # seg_b = np.logical_and(nu > nu_m, nu < nu_a)
+        seg_b = nu < nu_a
+        seg_c = np.logical_and(nu_c > nu, nu > nu_a)
+        seg_d = nu > nu_c
+
+        # Calculate the sharply-broken flux
+        flux = np.empty(nu.size)
+        # flux[seg_a] = f_peak * (nu[seg_a] / nu_m) ** b1
+        flux[seg_b] = f_peak * (nu[seg_b] / nu_c) ** b1
+        flux[seg_c] = f_peak * (nu[seg_c] / nu_c) ** b2
+        flux[seg_d] = f_peak * (nu_m / nu_c) ** b2 * (nu[seg_d] / nu_m) ** b3
+
+        # Plot the two
+        plt.vlines(nu_a, ymin=0.0, ymax=f_peak, color='black', linestyle='--', alpha=0.6)
+        plt.vlines(nu_c, ymin=0.0, ymax=f_peak * (nu_c / nu_a) ** b2, color='black', linestyle='--', alpha=0.6)
+        plt.title(r'Fast Cooling Spectral Flux $(\nu_m < \nu_a < \nu_c)$')
+        plt.xlabel(r'$\nu$ [Hz]')
         plt.loglog(nu, smoothed_flux, label='Smoothly Broken')
         plt.loglog(nu, flux, label='Sharply Broken')
         plt.legend()