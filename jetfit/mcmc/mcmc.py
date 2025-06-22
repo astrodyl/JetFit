Fix Slop Calculation

jetfit/mcmc/mcmc.py
@@ -99,18 +99,27 @@ class PTSampler:
     log_l_kwargs, log_p_kwargs : array_like, optional
         The log likelihood and log prior kwargs.
 
+    pool : optional
+        An object with a ``map`` method that follows the
+        same calling sequence as emcee's built-in ``map``
+        function. This is generally used to compute the
+        log-probabilities in parallel.
+
     kwargs
         Any kwargs to be passed to the sampler.
     """
     def __init__(
         self, ntemps, nwalkers, ndim, log_like, log_prior,
-        log_l_args=(), log_p_args=(), log_l_kwargs=(), log_p_kwargs=(), **kwargs
+        log_l_args=(), log_p_args=(), log_l_kwargs=(), log_p_kwargs=(),
+        pool=None, **kwargs
     ):
+        mapper = pool.map if pool is not None else map
+
         # Initialize the sampler
         self._sampler = ptemcee.Sampler(
             nwalkers, ndim, log_like, log_prior,
             log_l_args, log_p_args, log_l_kwargs, log_p_kwargs,
-            ptemcee.make_ladder(ndim, ntemps), **kwargs
+            ptemcee.make_ladder(ndim, ntemps), mapper=mapper, **kwargs
         )
         self._chain = None
         self._iteration = 0
@@ -143,14 +152,6 @@ class PTSampler:
     def ndim(self):
         return self._ndim
 
-    def validate(self):
-        """ placeholder """
-        raise NotImplementedError
-
-    def set_start_pos(self):
-        """ placeholder """
-        raise NotImplementedError
-
     def run_mcmc(self, x0, iterations, **kwargs):
         """
         Perform MCMC sampling.
@@ -173,7 +174,7 @@ class PTSampler:
         np.ndarray with shape [ntemps, nwalkers, ndim]
             The last samples.
         """
-        self._chain = self.sampler.chain(x0)
+        self._chain = self.sampler.chain(x0, **kwargs)
         self._chain.run(iterations)
         self._iteration = self._chain.length
 
@@ -206,6 +207,48 @@ class PTSampler:
                 'called `run_mcmc` yet?'
             )
         return self.chain.x[-1]
+
+    def draw_positions(self, params, models) -> np.ndarray:
+        """
+        Draw the initial positions from the priors.
+
+        PTSampler requires that the start positions be
+        valid (i.e., log posterior is finite). To meet
+        this, any invalid position is overwritten with
+        the best position for that temperature.
+
+        Parameters
+        ----------
+        params : Parameters
+            The MCMC parameters.
+
+        models : MCMCModel
+            The MCMC models.
+
+        Returns
+        -------
+        np.ndarray of float with shape [ntemps, nwalkers, ndim]
+            The starting positions.
+        """
+        # Draw starting positions
+        pos = np.zeros((self.ntemps, self.nwalkers, self.ndim))
+
+        for i in range(self.ntemps):
+            for j, p in enumerate(params.fitting):
+                pos[i, :, j] = p.prior.draw(self.nwalkers)
+
+        # Overwrite with each temperature's best position
+        log_p = np.full((self.ntemps, self.nwalkers), -np.inf)
+
+        for i in range(self.ntemps):
+            for j in range(self.nwalkers):
+                log_p[i, j] = log_posterior_fn(pos[i, j], params, models)  # type: ignore
+
+            pos[i][np.isinf(log_p[i])] = np.array(
+                pos[i][np.nanargmax(log_p[i])], copy=True
+            )
+
+        return pos
 
     def get_value(self, name, flat=False, thin=1, discard=0, temp=0):
         """
@@ -326,6 +369,38 @@ class PTSampler:
         return self.get_value("logP", **kwargs)
 
 
+class EnsembleSampler(emcee.EnsembleSampler):
+    """
+    Adapter for ``emcee.EnsembleSampler``.
+    """
+    def __init__(self, nwalkers, ndim, log_prob_fn, args, **kw):
+        super().__init__(nwalkers, ndim, log_prob_fn, args=args, **kw)
+
+    def draw_positions(self, params, **kwargs) -> np.ndarray:
+        """
+        Draw the initial positions from the priors.
+
+        Parameters
+        ----------
+        params : Parameters
+            The MCMC parameters.
+
+        kwargs :
+            For compatability with ``PTSampler.draw_positions``.
+
+        Returns
+        -------
+        np.ndarray of float with shape [nwalkers, ndim]
+            The starting positions.
+        """
+        pos = np.zeros((self.nwalkers, self.ndim))
+
+        for i, p in enumerate(params.fitting):
+            pos[:, i] = p.prior.draw(self.nwalkers)
+
+        return pos
+
+
 class MCMC:
     """
     Performs MCMC sampling.
@@ -351,64 +426,13 @@ class MCMC:
         self.sampler = None
         self.burn_chain = None
 
-        self.param_pos = None
         self.start_burn_pos = None
         self.start_run_pos = None
 
-        # Setters
-        self.set_param_position()
-
-    # <editor-fold desc="Getters and Setters">
     @property
     def ndim(self):
         """ The number of fitting dimensions. """
         return len(self.params.fitting)
-
-    def set_param_position(self) -> None:
-        """ Sets the position of the parameters in the fitting list. """
-        self.param_pos = {p.name : i for i, p in enumerate(self.params.fitting)}
-
-    def set_start_positions(self, nwalkers, ntemps=None):
-        """
-        Sets the starting burn-in positions.
-
-        Parameters
-        ----------
-        nwalkers : int
-            The number of walkers.
-
-        ntemps : int, optional, default=None
-            The number of temperatures for ``ptemcee``.
-        """
-        # PTSampler shape == (ntemps, nwalkers, ndim)
-        if isinstance(self.sampler, PTSampler):
-            self.start_burn_pos = np.zeros((ntemps, nwalkers, self.ndim))
-
-            for i in range(ntemps):
-                for j, p in enumerate(self.params.fitting):
-                    self.start_burn_pos[i, :, j] = p.prior.draw(nwalkers)
-
-            # PTSampler requires that the initial posterior be valid.
-            # Check every position for non-validity and replace with
-            # the best position for each temp level.
-            log_p = np.full((ntemps, nwalkers), -np.inf)
-            for i in range(ntemps):
-                for j in range(nwalkers):
-                    log_p[i, j] = log_posterior(
-                        self.start_burn_pos[i, j], self.params, self.models  # type: ignore
-                    )
-
-                # Overwrite invalid posteriors
-                best = np.nanargmax(log_p[i])
-                self.start_burn_pos[i][np.isinf(log_p[i])] = np.array(
-                    self.start_burn_pos[i][best], copy=True
-                )
-
-        else:  # Ensemble shape == (ntemps, nwalkers, ndim)
-            self.start_burn_pos = np.zeros((nwalkers, self.ndim))
-
-            for i, p in enumerate(self.params.fitting):
-                self.start_burn_pos[:, i] = p.prior.draw(nwalkers)
 
     def get_best_params(self, as_dict=True, **kwargs):
         """
@@ -435,39 +459,8 @@ class MCMC:
         max_index = np.nanargmax(self.sampler.get_log_prob(flat=True))
         params = self.sampler.get_chain(flat=True)[max_index]
         return self.params.samples_to_dict(params, **kwargs) if as_dict else params
-    # </editor-fold>
 
     # <editor-fold desc="Sampling Routine">
-    @staticmethod
-    def _validate_ptemcee(ntemps, pool=None):
-        """"""
-        if ntemps is None:
-            raise ValueError(
-                "Sampler `parallel_tempered` requires arge `ntemps`."
-            )
-
-    @staticmethod
-    def _validate_emcee(ntemps=None):
-        """"""
-        if ntemps is not None:
-            raise ValueError(
-                "Sampler `ensemble` does not use `ntemps`."
-            )
-
-    def _validate_sampler(self, sampler, pool=None, ntemps=None):
-        """"""
-        if sampler not in ('ensemble', 'parallel_tempered'):
-            raise ValueError(
-                f'Unexpected sampler name: {sampler}. '
-                f'Must be either `ensemble` or `parallel_tempered`.'
-            )
-
-        if sampler == 'parallel_tempered':
-            self._validate_ptemcee(ntemps, pool)
-
-        if sampler == 'ensemble':
-            self._validate_emcee(ntemps)
-
     def set_sampler(self, sampler, nwalkers, pool, ntemps=None, **kwargs):
         """
         Sets the sampler. Duh.
@@ -475,7 +468,7 @@ class MCMC:
         Parameters
         ----------
         sampler : str
-            The sampler type. Must be ``emcee`` or ``ptemcee``.
+            The sampler type. Must be ``ensemble`` or ``parallel_tempered``.
 
         nwalkers : int
             The number of walkers.
@@ -485,32 +478,22 @@ class MCMC:
             See ``get_pool_context()`` for details.
 
         ntemps : int, optional
-            The number of temperatures for ``ptemcee``.
+            The number of temperatures for ``parallel_tempered``.
 
         kwargs
             Any kwargs to be passed to the sampler.
         """
-        self._validate_sampler(sampler, pool, ntemps)
-
         if sampler == 'ensemble':
-            if isinstance(pool, type(nullcontext())):
-                # Single threaded/processed
-                self.sampler = emcee.EnsembleSampler(
-                    nwalkers, self.ndim, log_posterior,
-                    args=(self.params, self.models), **kwargs  # type: ignore
-                )
-            else:
-                # Multithreaded/processed
-                self.sampler = emcee.EnsembleSampler(
-                    nwalkers, self.ndim, log_posterior,
-                    args=(self.params, self.models), pool=pool, **kwargs # type: ignore
-                )
+            self.sampler = EnsembleSampler(
+                nwalkers, self.ndim, log_posterior_fn,
+                args=(self.params, self.models), pool=pool, **kwargs # type: ignore
+            )
 
         elif sampler == 'parallel_tempered':
-            # PTSampler is single processed only
             self.sampler = PTSampler(
-                ntemps, nwalkers, self.ndim, log_likelihood, log_prior,
-                log_l_args=(self.params, self.models), log_p_args=(self.params,)
+                ntemps, nwalkers, self.ndim, log_likelihood_fn, log_prior_fn,
+                log_l_args=(self.params, self.models), log_p_args=(self.params,),
+                pool=pool, **kwargs
             )
 
     def run(
@@ -534,13 +517,13 @@ class MCMC:
             before resetting it for the main run.
 
         sampler : str, optional, default='ensemble'
-            Must be either `ensemble` or `parallel_tempered`.
+            Must be ``ensemble`` or ``parallel_tempered``.
 
         workers : int, optional, default=None
             The max number of workers to use.
 
         ntemps : int, optional, default=None
-            The number of temperatures to use for ``PTSampler``.
+            The number of temperatures for ``PTSampler``.
 
         sampler_kw : dict, optional
             Any kwargs to pass to the sampler.
@@ -549,11 +532,20 @@ class MCMC:
             Any kwargs to pass to the ``run_mcmc`` method.
         """
         with get_pool_context(workers) as pool:
-            self.set_sampler(sampler, nwalkers, pool, ntemps, **(sampler_kw or {}))
-            self.set_start_positions(nwalkers, ntemps)
+            self.set_sampler(
+                sampler, nwalkers, pool, ntemps, **(sampler_kw or {})
+            )
 
-            if burn > 0:
-                print('burning')
+            start_pos = self.sampler.draw_positions(
+                params=self.params, models=self.models
+            )
+
+            if burn < 1:
+                self.start_run_pos = start_pos
+
+            else:
+                self.start_burn_pos = start_pos
+
                 # Run burn in and save the last position
                 self.start_run_pos = (
                     self.sampler.run_mcmc(
@@ -568,7 +560,6 @@ class MCMC:
                 self.sampler.reset()
 
             # Run production
-            print('running')
             self.sampler.run_mcmc(
                 self.start_run_pos, iterations, **(run_kw or {})
             )
@@ -714,11 +705,13 @@ class MCMCModels:
             **p.get('init')).extinguish(wn, **p.get('eval'))
 
 
+# https://emcee.readthedocs.io/en/stable/tutorials/parallel/
 # For multiprocessing purposes, emcee requires that methods and
 # arguments be pickle-able. As such, the methods below are made
 # global to meet this requirement.
 
-def log_prior(theta, params) -> float:
+
+def log_prior_fn(theta, params) -> float:
     """
     Evaluates the natural log of the priors.
 
@@ -747,7 +740,7 @@ def log_prior(theta, params) -> float:
     return lp
 
 
-def log_likelihood(theta, params, models) -> float:
+def log_likelihood_fn(theta, params, models) -> float:
     """
     Calculates the natural log of the likelihood.
 
@@ -783,13 +776,13 @@ def log_likelihood(theta, params, models) -> float:
     )
 
     # Format the slop (if using)
-    s = slop(p.get('slop').get('slop'), models.obs)
+    s = slop(p.get('slop'), models.obs)
 
     # return log likelihood
     return -0.5 * chi_squared(modeled, models.obs, s)  # type: ignore
 
 
-def log_posterior(theta, params, models) -> float:
+def log_posterior_fn(theta, params, models) -> float:
     """
     Calculates the natural log of the posterior
     probability.
@@ -814,8 +807,8 @@ def log_posterior(theta, params, models) -> float:
     float
         The natural log of the posterior.
     """
-    if np.isfinite(lp := log_prior(theta, params)):
-        ll = log_likelihood(theta, params, models)
+    if np.isfinite(lp := log_prior_fn(theta, params)):
+        ll = log_likelihood_fn(theta, params, models)
 
         if np.isfinite(ll):
             return lp + ll
@@ -850,7 +843,22 @@ def calibration_offsets(modeled, offsets, pos) -> np.ndarray:
 
 
 def slop(s, obs) -> float | np.ndarray | None:
-    """"""
+    """
+    Formats for slop to the modeled data.
+
+    Parameters
+    ----------
+    s : float or dict
+        The slop value or grouped slop values.
+
+    obs : Observation
+        The observational data.
+
+    Returns
+    -------
+    float or np.ndarray or None
+        The slop value(s).
+    """
     if isinstance(s, (int, float)):
         return s
 

jetfit/models/fireball.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.optimize import root_scalar
 
 from jetfit.core.input import Observation
 from jetfit.models.basemodels import BlastWaveModel, ObservedSpectrumModel
@@ -7,19 +6,17 @@ from jetfit.models.basemodels import AbsorptionFrequencyModel, BaseFireballModel
 from jetfit.models.basemodels import SynchrotronFrequencyModel
 from jetfit.models.basemodels import CoolingFrequencyModel, PeakFluxModel
 
-# ignore `dust_extinction` user warnings
-import warnings
-warnings.filterwarnings('ignore', category=UserWarning)
-
 
 class StratifiedFireballModel(BaseFireballModel):
     """
-    Implements the ultra-relativistic shock moving into an
-    external medium with density rho = rho_0 * R^-k.
+    A fully analytic description of an ultra-relativistic
+    shock moving into an external stratified medium with
+    density rho = rho_0 * R^-k. Where k transitions from
+    one asymptotic value to another.
 
     Parameters
     ----------
-    E : float or astropy.units.Quantity
+    E : float
         The explosion energy normalized to 1e52 ergs.
 
     p : float
@@ -27,19 +24,22 @@ class StratifiedFireballModel(BaseFireballModel):
 
     eps_b : float
         The fraction of thermal energy in the magnetic field.
+        Must be in the range [0, 1].
 
     eps_e : float
         The fraction of thermal energy carried by relativistic
-        electrons.
+        electrons. Must be in the range [0, 1].
 
     z : float
         The redshift to the event.
 
-    dL : float or astropy.units.Quantity
-        The luminosity distance to the event [1e28 cm].
+    dL : float
+        The luminosity distance to the event [1e28 cm]. Requiring
+        the distance to be provided in addition to the redshift
+        prevents the need to assume a cosmology here.
 
     sn, sni : float, optional
-        The density smoothing factor. `sni` is the inverse of the
+        The density smoothing factor. ``sni`` is the inverse of the
         smoothing factor. Useful for changing fitting basis in MCMC.
 
     k1 : float
@@ -51,17 +51,17 @@ class StratifiedFireballModel(BaseFireballModel):
     X : float
         The hydrogen mass fraction.
 
-    tj : float, optional
-        The jet break time in days.
+    tj : float, optional, default=None
+        The jet break observer-frame time [d].
 
-    sj, sji : float, optional
+    sj, sji : float, optional, default=None
         The jet break smoothing factor. `sji` is the inverse of the
         smoothing factor. Useful for changing fitting basis in MCMC.
+        Required if ``tj != None``.
 
     use_sa : bool, optional, default=True
         Should self-absorption be modeled?
     """
-
     # noinspection PyPep8Naming
     def __init__(
             self, E, p, eps_b, eps_e, z, dL, nt, rt, X,
@@ -90,96 +90,6 @@ class StratifiedFireballModel(BaseFireballModel):
             return False
 
         return super().is_valid and abs(self.sn) > 0.1
-
-    # def smooth2(self, times):
-    #     """"""
-    #     ts = np.atleast_1d(times)
-    #
-    #     def alpha(k):
-    #         """"""
-    #         return 16 / (17 - 4 * k)
-    #
-    #     def beta(k):
-    #         """"""
-    #         return 4 - k
-    #
-    #     def rho0(r, k):
-    #         """"""
-    #         return 1.67e-24 * n_eff(r) * r ** k
-    #
-    #     def k_eff(r):
-    #         """ Effective density power-law index. """
-    #         x = r / self.rt
-    #         k_eff_num = self.k1 * x ** (self.k1 * self.sn) + self.k2 * x ** (self.k2 * self.sn)
-    #         k_eff_den = x ** (self.k1 * self.sn) + x ** (self.k2 * self.sn)
-    #         return k_eff_num / k_eff_den
-    #
-    #     def n_eff(r):
-    #         """ Effective number density."""
-    #         x = r / self.rt
-    #         return self.nt * (2 ** (1 / self.sn)) * (x ** (self.k1 * self.sn) + x ** (self.k2 * self.sn)) ** -(1 / self.sn)
-    #
-    #     def r_model(r, t):
-    #         """"""
-    #         E = 1e52 * self.E
-    #         return (beta(k_eff(r)) * E * t) ** (1 / (4 - k_eff(r))) * (alpha(k_eff(r)) * np.pi * rho0(r, k_eff(r)) * 2.99e10) ** (-1 / (4 - k_eff(r)))
-    #
-    #     def F(r, t):
-    #         return r_model(r, t) - r
-    #
-    #     r_of_t = []
-    #
-    #     for time in (ts * 86_400):
-    #         sol = root_scalar(F, args=(time,), bracket=[1e16, 1e21], method='brentq')
-    #         r_of_t.append(sol.root)
-    #     r_of_t = np.array(r_of_t)
-    #
-    #     return n_eff(r_of_t) * ((r_of_t / self.rt)** k_eff(r_of_t)), k_eff(r_of_t)
-    #
-    # def radii2(self, times):
-    #     """
-    #     """
-    #     ts = np.atleast_1d(times)
-    #
-    #     def alpha(k):
-    #         """"""
-    #         return 16 / (17 - 4 * k)
-    #
-    #     def beta(k):
-    #         """"""
-    #         return 4 - k
-    #
-    #     def rho0(r, k):
-    #         """"""
-    #         return 1.67e-24 * n_eff(r) * r ** k
-    #
-    #     def k_eff(r):
-    #         """ Effective density power-law index. """
-    #         x = r / self.rt
-    #         k_eff_num = self.k1 * x ** (self.k1 * self.sn) + self.k2 * x ** (self.k2 * self.sn)
-    #         k_eff_den = x ** (self.k1 * self.sn) + x ** (self.k2 * self.sn)
-    #         return k_eff_num / k_eff_den
-    #
-    #     def n_eff(r):
-    #         """ Effective number density."""
-    #         x = r / self.rt
-    #         return self.nt * (2 ** (1 / self.sn)) * (x ** (self.k1 * self.sn) + x ** (self.k2 * self.sn)) ** -(
-    #                     1 / self.sn)
-    #
-    #     def r_model(r, t):
-    #         """"""
-    #         E = 1e52 * self.E
-    #         return (beta(k_eff(r)) * E * t) ** (1 / (4 - k_eff(r))) * (
-    #                     alpha(k_eff(r)) * np.pi * rho0(r, k_eff(r)) * 2.99e10) ** (-1 / (4 - k_eff(r)))
-    #
-    #     def F(r, t):
-    #         return r_model(r, t) - r
-    #
-    #     r_of_t = []
-    #     for time in (ts * 86_400):
-    #         sol = root_scalar(F, args=(time,), bracket=[1e16, 1e21], method='brentq')
-    #         r_of_t.append(sol.root)
-    #     return np.array(r_of_t)
 
     def smooth(self, t):
         """
@@ -221,9 +131,9 @@ class StratifiedFireballModel(BaseFireballModel):
     def radii(self, t):
         """
         Calculates the radius traversed by the blast wave
-        during time `t` in a stratified medium defined by
-        the power-law indices `k1` and `k2`, and the radius
-        and density at the transition, `nt` and `rt`.
+        during time ``t`` in a stratified medium defined by
+        the power-law indices ``k1`` and ``k2``, and the radius
+        and density at the transition, ``nt`` and ``rt``.
 
         Parameters
         ----------
@@ -250,17 +160,17 @@ class StratifiedFireballModel(BaseFireballModel):
 
     def model(self, obs: Observation):
         """
-        Models an `observation` object.
+        Models the observational data, ``obs``.
 
         Parameters
         ----------
         obs : Observation
-            The observation object to model.
+            The observational data.
 
         Returns
         -------
         np.ndarray of float
-            The unextinguished modeled flux.
+            The modeled observational data.
         """
         if not self.is_valid:
             return np.array([np.nan])
@@ -409,15 +319,15 @@ class StratifiedFireballModel(BaseFireballModel):
             The effective density power-law indices.
 
         nu_m : float or np.ndarray of float, optional
-            The synchrotron frequencies [Hz] at time `t`.
+            The synchrotron frequencies [Hz] at time ``t``.
 
         nu_c : float or np.ndarray of float, optional
-            The cooling frequencies [Hz] at time `t`.
+            The cooling frequencies [Hz] at time ``t``.
 
         Returns
         -------
         float or np.ndarray of float
-            The self-absorption frequency [Hz] at time `t`.
+            The self-absorption frequency [Hz] at time(s) ``t``.
         """
         if n is None or k is None:
             n, k = self.smooth(t)
@@ -448,39 +358,35 @@ class StratifiedFireballModel(BaseFireballModel):
 
 class FireballModel(BaseFireballModel):
     """
-    Implements the ultra-relativistic shock moving into an
-    external medium with density rho = rho_0 * R^-k.
-
-    Parameters can be passed either as an Astropy Quantity with
-    an associated unit, or as a simple float. However, they will
-    only be stored as floats. Quantities will be converted to the
-    appropriate units before storing the float value. This is done
-    because operating with Astropy Quantities is very slow and makes
-    MCMC fitting extremely difficult. If a float is passed, it is
-    assumed that the value is already in the expected units.
+    A fully analytic description of an ultra-relativistic
+    shock moving into an external medium with density
+    rho = rho_0 * R^-k.
 
     Parameters
     ----------
-    E : float or astropy.units.Quantity
+    E : float
         The explosion energy normalized to 1e52 ergs.
 
     p : float
-        The electron energy index (dimensionless).
+        The electron energy index.
 
     eps_b : float
         The fraction of thermal energy in the magnetic field.
+        Must be in the range [0, 1].
 
     eps_e : float
         The fraction of thermal energy carried by relativistic
-        electrons.
+        electrons. Must be in the range [0, 1].
 
     z : float
-        The redshift to the event.
+        The redshift of the event.
 
-    dL : float or astropy.units.Quantity
-        The luminosity distance to the event.
+    dL : float
+        The luminosity distance to the event [1e28 cm]. Requiring
+        the distance to be provided in addition to the redshift
+        prevents the need to assume a cosmology here.
 
-    rho0 : float or astropy.units.Quantity
+    rho0 : float
         The density normalization.
 
     k : float
@@ -489,21 +395,15 @@ class FireballModel(BaseFireballModel):
     X : float
         The hydrogen mass fraction.
 
-    tj : float, optional
-        The jet break time in days.
+    tj : float, optional, default=None
+        The jet break observer-frame time [d].
 
-    sj : float, optional
-        The jet break smoothing factor.
+    sj : float, optional, default=None
+        The jet break smoothing factor. Required if ``tj != None``.
 
     use_sa : bool, optional, default=True
         Should self-absorption be modeled?
-
-    References
-    ----------
-    [1] Broadband view of blast wave physics: A study
-        of gamma-ray burst afterglows
     """
-
     # noinspection PyPep8Naming
     def __init__(self, E, p, eps_b, eps_e, z, dL, rho0, k, X, tj=None, sj=None, sji=None, use_sa=True):
         super().__init__(E, p, eps_b, eps_e, z, dL, X, tj, sj, sji, use_sa)
@@ -513,25 +413,25 @@ class FireballModel(BaseFireballModel):
 
     def model(self, obs: Observation, subset: np.ndarray = None):
         """
-        Models an `observation` object.
+        Models the observational data, ``obs``.
 
         Parameters
         ----------
         obs : Observation
-            The observation object to model.
+            The observational data.
 
         subset : np.ndarray of bool, optional
-            The truth array of which values to model.
+            Models data where ``subset==True``.
 
         Returns
         -------
         np.ndarray of float
-            The unextinguished modeled flux.
+            The modeled observational data.
         """
         if not self.is_valid:
             return np.array([np.nan])
 
-        # Model the smoothed, unextinguished flux
+        # return the modeled observational data
         return ObservedSpectrumModel(**self.spectrum(obs.times()),
             jet=self.jet_break(obs.times()), arrays=obs.as_arrays
         ).model(subset)
@@ -619,13 +519,16 @@ class FireballModel(BaseFireballModel):
         """
         Calculates the self-absorption frequency.
 
-        Since the self-absorption frequency has different relations
-        depending on its relative position to the other critical
-        frequencies, I calculate the self-absorption frequency
-        for both slow-cooling cases (nu_m < nu_a and nu_a < nu_m).
+        The self-absorption frequency has a circular definition.
+        For example, to calculate nu_a, you must first know how
+        nu_a relates to the nu_m and nu_c, but nu_a isn't known
+        because it needs to be known before it can be known >:)
 
-        The result is a combined array where the self-absorption
-        is compared to the synchrotron frequency.
+        This solution is weak, but the self-absorption frequency
+        is calculated for every case (except nu_a > both nu_c and
+        nu_m, not supported). The result is a combined array where
+        the self-absorptions are compared to the synchrotron and
+        cooling frequencies.
 
         Parameters
         ----------

jetfit/models/jetsim.py
@@ -154,7 +154,7 @@ class JetSimpy:
                 ifm = np.logical_and(ifm, subset)
 
             res[ifm] = self.integrated_flux(
-                obs.times()[ifm], obs.int_lower()[ifm], obs.int_upper()[ifm]
+                obs.times()[ifm], obs.int_lowers()[ifm], obs.int_uppers()[ifm]
             )
 
         # Model the spectral indices
@@ -163,7 +163,7 @@ class JetSimpy:
                 sim = np.logical_and(sim, subset)
 
             res[sim] = self.spectral_index(
-                obs.times()[sim], obs.int_lower()[sim], obs.int_upper()[sim]
+                obs.times()[sim], obs.int_lowers()[sim], obs.int_uppers()[sim]
             )
 
         return res